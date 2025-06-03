615c930220a00c3eb7e001b7cd37ab0eab061731
Wanderer <82797317+wanderer00a1@users.noreply.github.com>
2025-06-03 18:10:40 +0530

    Update authController.ts

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express.js";
-import bcrypt from "bcryptjs.js";
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
 import User from "../models/userModel.js";
 import { generateToken } from "../lib/utils.js";

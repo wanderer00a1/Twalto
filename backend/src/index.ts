cde2012ba865451269f234dd3de2929359973a81
wanderer <wanderer00a1@users.noreply.github.com>
2025-06-03 16:21:12 +0530

    Final Ver

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,20 @@
 import express from "express";
 
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db";
+import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import authRoutes from "./routes/authRoute";
-import messageRoutes from "./routes/messageRoute";
-import { app, server } from "./lib/socket";
+import path from "path";
+
+import authRoutes from "./routes/authRoute.js";
+import messageRoutes from "./routes/messageRoute.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,11 +25,16 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Working.....");
-});
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is Running on port " + PORT);
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -32,7 +32,8 @@ type AuthStore = {
   disconnectSocket: () => void;
 };
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create<AuthStore>((set, get) => ({
   authUser: null,

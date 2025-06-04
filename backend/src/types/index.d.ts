7eb64d91f80d2d58ee836f49d66641af7eaa85d5
wanderer <wanderer00a1@users.noreply.github.com>
2025-06-04 10:51:50 +0530

    model issue fixed

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,7 +1,7 @@
-import { Document } from "mongoose";
+import { Document, Types } from "mongoose";
 
 interface IUser extends Document {
-  _id: string;
+  _id: Types.ObjectId;
   username: string;
   email: string;
   password: string;
